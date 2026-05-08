d963a9ab34d012492697c314cd549b35120bf0eb
Muhammad Aneeq <muhammadaneeq.769@gmail.com>
2026-05-08 22:04:04 +0500

    updated

--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import PasswordLoginView, VerifyLoginOTPView
-
-
-urlpatterns = [
-    path("login/password/", PasswordLoginView.as_view(), name="password-login"),
-    path("login/verify-otp/", VerifyLoginOTPView.as_view(), name="verify-login-otp"),
-]
